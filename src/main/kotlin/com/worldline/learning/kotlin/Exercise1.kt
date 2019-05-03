4ea53e941fb43fdc853e4ec692b860d6f5bc08d2
Antoine Neveux <antoine.neveux@worldline.com>
2019-05-03 13:49:40 +0200

    :sparkles: adding some maven template project for preparing the practical works

---
+++ src/main/kotlin/com/worldline/learning/kotlin/Exercise1.kt
@@ -0,0 +1,5 @@
+package com.worldline.learning.kotlin
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/test/kotlin/com/worldline/learning/kotlin/Exercise1Test.kt
@@ -0,0 +1,13 @@
+package com.worldline.learning.kotlin
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class Exercise1Test {
+
+    @Test
+    fun testFoo() {
+        assertTrue(true)
+    }
+
+}
