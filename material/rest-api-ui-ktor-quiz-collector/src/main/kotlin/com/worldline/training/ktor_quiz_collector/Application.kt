78ca96a4ca5a12e2d8bf95824a737e75c4297c33
yostane <1958676+yostane@users.noreply.github.com>
2024-08-30 17:33:05 +0200

    More endpoints

--- material/rest-api-ui-ktor-quiz-collector/src/main/kotlin/com/worldline/training/ktor_quiz_collector/Application.kt
+++ material/rest-api-ui-ktor-quiz-collector/src/main/kotlin/com/worldline/training/ktor_quiz_collector/Application.kt
@@ -4,9 +4,9 @@ import com.worldline.training.ktor_quiz_collector.plugins.configureHTTP
 import com.worldline.training.ktor_quiz_collector.plugins.configureRouting
 import com.worldline.training.ktor_quiz_collector.plugins.configureSerialization
 import com.worldline.training.ktor_quiz_collector.plugins.configureTemplating
+import com.worldline.training.ktor_quiz_collector.routes.configureHelloRoutes
 import com.worldline.training.ktor_quiz_collector.routes.configureMiscRoutes
 import com.worldline.training.ktor_quiz_collector.routes.configureQuizCollector
-import com.worldline.training.ktor_quiz_collector.utils.addSampleValues
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -22,5 +22,6 @@ fun Application.module() {
     configureSerialization()
     configureRouting()
     configureMiscRoutes()
+    configureHelloRoutes()
     configureQuizCollector()
 }
--- material/rest-api-ui-ktor-quiz-collector/src/main/kotlin/com/worldline/training/ktor_quiz_collector/routes/MiscRoutes.kt
+++ material/rest-api-ui-ktor-quiz-collector/src/main/kotlin/com/worldline/training/ktor_quiz_collector/routes/MiscRoutes.kt
@@ -5,14 +5,63 @@ import com.worldline.training.ktor_quiz_collector.utils.getCorrectStats
 import com.worldline.training.ktor_quiz_collector.utils.quizResponses
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.css.*
+import kotlinx.css.Color
+import kotlinx.css.Display
+import kotlinx.css.FlexWrap
+import kotlinx.css.Gap
+import kotlinx.css.JustifyContent
+import kotlinx.css.TextAlign
+import kotlinx.css.backgroundColor
+import kotlinx.css.body
+import kotlinx.css.border
+import kotlinx.css.display
+import kotlinx.css.flexWrap
+import kotlinx.css.gap
+import kotlinx.css.h1
+import kotlinx.css.justifyContent
+import kotlinx.css.margin
+import kotlinx.css.marginTop
+import kotlinx.css.p
+import kotlinx.css.padding
 import kotlinx.css.properties.boxShadow
+import kotlinx.css.px
+import kotlinx.css.rem
+import kotlinx.css.svg
+import kotlinx.css.textAlign
+import kotlinx.html.body
+import kotlinx.html.h1
+
+fun Application.configureHelloRoutes() {
+    routing {
+        get("/hello") {
+            call.respondHtml {
+                body {
+                    h1 { +"Hello Ktor" }
+                }
+            }
+        }
+
+        get("/data") {
+            call.respond(quizResponses)
+        }
+    }
+}
 
 fun Application.configureMiscRoutes() {
     routing {
+        get("/responses") {
+            call.respond(quizResponses.map { quizResponse ->
+                quizResponse.responses.fold(mutableMapOf<String, String>()) { acc, element ->
+                    acc[element.question] = element.answer
+                    acc
+                }
+            })
+        }
+
         staticResources("/static", "static")
 
         get("/styles-graph.css") {
@@ -74,6 +123,10 @@ fun Application.configureMiscRoutes() {
             val result = quizResponses.flatMap { it.responses }.groupBy { it.question }
                 .mapValues { it.value.map { value -> value.answer } }
             call.respond(result)
+        }
+
+        get("/table2") {
+            call.respond(quizResponses)
         }
     }
 }
